bd9728cabd7b5647cb43967bb912fcd83afe10fd
ciberneticaDieu <99042383+ciberneticaDieu@users.noreply.github.com>
2022-03-16 18:44:12 +0330

    Add files via upload

--- quera/Speaker.py
+++ quera/Speaker.py
@@ -1,4 +1,6 @@
-sound=input()
+# https://quera.org/problemset/3430/
+# python 3.10.2
+sound = input()
 print(sound)
-for i in range(1,len(sound)):
+for i in range(1, len(sound)):
     print(sound[i]*i+sound[i:])
