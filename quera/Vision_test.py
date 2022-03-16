87c0271eb9fcf95e64a8e7f380418802a5d4b6d9
ciberneticaDieu <99042383+ciberneticaDieu@users.noreply.github.com>
2022-03-16 19:01:07 +0330

    Add files via upload

--- quera/Vision_test.py
+++ quera/Vision_test.py
@@ -1,12 +1,14 @@
-num=int(input())
-string1=str(input())
-string2=str(input())
-n=0
+# https://quera.org/problemset/2659/
+# python 3.10.2
+num = int(input())
+string1 = str(input())
+string2 = str(input())
+n = 0
 for i in range(len(string1)):
     for x in string1[i]:
         for y in string2[i]:
             if y == x:
-               pass
+                pass
             else:
-                n+=1 
+                n += 1
 print(n)
