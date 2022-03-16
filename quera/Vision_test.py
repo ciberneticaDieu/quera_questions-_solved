273a7c3366c362ffc5c9b45d83a3e5c79b2c2739
ciberneticaDieu <99042383+ciberneticaDieu@users.noreply.github.com>
2022-03-16 18:59:26 +0330

    Add files via upload

---
+++ quera/Vision_test.py
@@ -0,0 +1,12 @@
+num=int(input())
+string1=str(input())
+string2=str(input())
+n=0
+for i in range(len(string1)):
+    for x in string1[i]:
+        for y in string2[i]:
+            if y == x:
+               pass
+            else:
+                n+=1 
+print(n)
